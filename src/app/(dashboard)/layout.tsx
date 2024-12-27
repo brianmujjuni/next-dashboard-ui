817f83b9d7d92ea43edcf289d7d686763ea713ba
Automex <automex@Automexs-MacBook-Pro.local>
2024-12-27 17:39:12 +0300

    resolve dashboard missing css

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -25,8 +25,8 @@ export default function DashboardPage({
         </Link>
         <Menu />
       </div>
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F78FA] overflow-scroll ">
-        <Navbar/>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar />
         {children}
       </div>
     </div>
